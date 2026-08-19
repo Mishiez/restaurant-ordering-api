55bc4b2bd6ab963ea9ce7b932717dcfb2bdbf460
Michelle Gathoni <michellegathonimain@gmail.com>
2026-08-19 21:19:39 +0300

    chore: register menuitem and order models with admin site

--- menu/admin.py
+++ menu/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import MenuItem
 
-# Register your models here.
+@admin.register(MenuItem)
+class MenuItemAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'price', 'available', 'created_at']
+    list_filter = ['available']
+    search_fields = ['name']
